c89586a596301abfb96e2b34cc9118f27d270ac6
Teddy-38 <Zhdanov_as94@mail.ru>
2025-01-23 20:49:06 +0800

    Task 1-5

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-
-
+        for (int i = 1; i <= 10; i = i + 1) {
+            System.out.println(i);
+        }
+        System.out.println("Task2");
+        for (int i = 10; i >= 1; i = i - 2) {
+            System.out.println(i);
+        }
+        System.out.println("Task3");
+        for (int i = 0; i <= 17; i = i + 2) {
+            System.out.println(i);
+        }
+        System.out.println("Task4");
+        for (int i = 10; i >= -10; i = i - 1) {
+            System.out.println(i);
+        }
+        System.out.println("Task5");
+        for (int i = 1904; i <= 2096; i = i + 4) {
+            System.out.println(i + "год является высокосным");
+        }
     }
 }
